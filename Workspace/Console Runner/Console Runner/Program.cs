64fd74626be5c5f153cb05936c064f2b192ad619
Matthew R Quinn <MatthewRileyQuinn@gmail.com>
2021-12-10 14:42:10 -0800

    Created workspace

---
+++ Workspace/Console Runner/Console Runner/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var connectionString = "Server = myServerAddress; Database = myDataBase; Uid = myUsername; Pwd = myPassword;";

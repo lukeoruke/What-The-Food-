088dedfce0795e2042d746b614fb518516bacd15
Tythor <tthorin@live.com>
2022-01-30 22:44:39 -0800

    History Class

---
+++ Milestone 3/Workspace/Console Runner/Console Runner/History.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Console_Runner
+{
+	public class History
+	{
+		private Queue<string> FoodItems;
+		private int queueSize;
+
+		public History()
+		{
+			FoodItems = new Queue<string>();
+			queueSize = 25;
+		}
+
+		public int getCount()
+        {
+			return FoodItems.Count;
+        }
+
+		public string remove()
+		{
+			return FoodItems.Dequeue();
+		}
+
+		public void add(string foodItem)
+        {
+			if(getCount() == queueSize)
+            {
+				remove();
+            }
+			FoodItems.Enqueue(foodItem);
+        }		
+	}
+}
+

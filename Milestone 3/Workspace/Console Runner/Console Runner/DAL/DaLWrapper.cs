94c1fa6df423cc26da8a9e0cfeb13b0abced9726
Matthew R Quinn <MatthewRileyQuinn@gmail.com>
2022-02-01 13:56:11 -0800

    added several essential functions to the Data access layer wrapper

--- Milestone 3/Workspace/Console Runner/Console Runner/DAL/DaLWrapper.cs
+++ Milestone 3/Workspace/Console Runner/Console Runner/DAL/DaLWrapper.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using User;
 
 namespace Console_Runner.DAL
 {
@@ -15,6 +16,8 @@ namespace Console_Runner.DAL
 
         }
 
+        /////////////////////////////////////////////////////////////Accounts////////////////////////////////////////////////////////////////////////////////////////////////
+
         public bool userExists(string email)
         {
             if (context.accounts.Find(email) != null)
@@ -23,6 +26,33 @@ namespace Console_Runner.DAL
             }
             return false;
         }
+
+        public Account getAccount(string email)
+        {
+            Account acc = context.accounts.Find(email);
+            if (acc != null)
+            {
+                return acc;
+            }
+            else
+            {
+                throw new Exception("account not found exception");
+            }
+
+        }
+        public bool addAccount(Account acc)
+        {
+            context.accounts.Add(acc);
+            context.SaveChanges();
+            return true;
+        }
+        public bool removeAccount(Account acc)
+        {
+            context.Remove(acc);
+            context.SaveChanges();
+            return true;
+        }
+        /////////////////////////////////////////////////////////////Permissions////////////////////////////////////////////////////////////////////////////////////////////////
         public bool hasPermission(string email, string permission)
         {
             if (context.permissions.Find(email, permission) != null)
@@ -31,5 +61,6 @@ namespace Console_Runner.DAL
             }
             return false ;
         }
+
     }
 }

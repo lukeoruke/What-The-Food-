f754b5219c0ad1782e170355a93ece5b01443302
alexyung <48598834+alexyung@users.noreply.github.com>
2022-02-14 22:31:08 -0800

    create folder for AMR; define AMR properties

---
+++ Milestone 3/Workspace/Console Runner/Console Runner/AMR/AMR.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Console_Runner.AMR
+{
+	public enum ActivityLevel
+    {
+		light,
+		moderate,
+		heavy
+    }
+	public class AMR
+	{
+		public bool IsMale { get; set; }
+		// weight, height, and age must be non-negative values.
+		public int Weight 
+		{ 
+			get; 
+			set
+            {
+				if (value > 0)
+                {
+					Weight = value;
+                }
+            }; 
+		}
+		public float Height 
+		{ 
+			get;
+            set
+            {
+				if (value > 0f)
+                {
+					Weight = value;
+                }
+            }; 
+		}
+		public int Age 
+		{ 
+			get; 
+			set
+            {
+				if(value > 0)
+                {
+					Age = value;
+                }
+            }; 
+		}
+		public ActivityLevel Activity { get; set; }
+		public bool IsCustomAMR { get; set; }
+		// CustomAMR should only be accessible if IsCustomAMR is true
+		public double CustomAMR 
+		{
+            get
+            {
+                if (IsCustomAMR)
+                {
+					return CustomAMR;
+                }
+				else
+                {
+					return -1;
+                }
+            }; 
+			set
+			{
+                if (IsCustomAMR)
+                {
+					CustomAMR = value;
+                }
+            }; 
+		}
+		public AMR()
+		{
+
+		}
+	}
+}
+

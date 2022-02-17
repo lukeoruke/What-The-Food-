2aa796037d79da41f684fac7458e6767280c8225
alexyung <48598834+alexyung@users.noreply.github.com>
2022-02-16 21:09:43 -0800

    implement calculateAMR()

--- Milestone 3/Workspace/Console Runner/Console Runner/AMR/AMR.cs
+++ Milestone 3/Workspace/Console Runner/Console Runner/AMR/AMR.cs
@@ -5,38 +5,46 @@ namespace Console_Runner.AMR
 	// internal enum - make private, protected, or public???
 	public enum ActivityLevel
     {
-		light,
-		moderate,
-		heavy
+		None,
+		Light,
+		Moderate,
+		Daily,
+		Heavy
     }
 
 	public class AMR
 	{
+		private int _weight;
+		private float _height;
+		private int _age;
+		private double _customAMR;
+
 		public bool IsMale { get; set; }
 		// weight, height, and age must be non-negative values.
-		public int Weight 
-		{ 
-			get; 
-			set
+		// assume properties are measured in metric units - kg, cm, etc.
+		public int Weight
+        {
+            get => _weight;
+            set
             {
-				if (value > 0) { Weight = value; }
-            }; 
+				if (value > 0) { _weight = value; }
+            } 
 		}
 		public float Height 
 		{ 
-			get;
+			get => _height;
             set
             {
-				if (value > 0f) { Weight = value; }
-            }; 
+				if (value > 0f) { _height = value; }
+            } 
 		}
 		public int Age 
 		{ 
-			get; 
+			get => _age; 
 			set
             {
-				if(value > 0) { Age = value; }
-            }; 
+				if(value > 0) { _age = value; }
+            } 
 		}
 		public ActivityLevel Activity { get; set; }
 		public bool IsCustomAMR { get; set; }
@@ -45,13 +53,12 @@ namespace Console_Runner.AMR
 		{
             get
             {
-                if (IsCustomAMR) { return CustomAMR; }
-				else { return -1; }
-            }; 
+				return IsCustomAMR ? CustomAMR : -1;
+            } 
 			set
 			{
                 if (IsCustomAMR) { CustomAMR = value; }
-            }; 
+            } 
 		}
 
 		// ctor
@@ -61,9 +68,34 @@ namespace Console_Runner.AMR
 		}
 
 		// methods
+
+		/// <summary>
+		/// <para>Calculates active metabolic rate (AMR) from this object's properties using the Harris-Benedict equation.
+		/// Refer to:
+		/// https://www.verywellfit.com/how-many-calories-do-i-need-each-day-2506873
+        /// to see the equation.
+		/// </para>
+		/// </summary>
+		/// <returns>A double representing the AMR calculated from the properties in this class.</returns>
 		public double CalculateAMR()
 		{
 			if (IsCustomAMR) { return CustomAMR; }
+            else
+            {
+				// if male, use first equation; if female, use second equation
+				double bmr = IsMale ? 66.47 + (13.75 * Weight) + (5.003 * Height) - (6.755 * Age) :
+									  655.1 + (9.563 * Weight) + (1.85 * Height) - (4.676 * Age);
+				double amr = Activity switch
+				{
+					ActivityLevel.None => bmr * 1.2,
+					ActivityLevel.Light => bmr * 1.375,
+					ActivityLevel.Moderate => bmr * 1.55,
+					ActivityLevel.Daily => bmr * 1.725,
+					ActivityLevel.Heavy => bmr * 1.9,
+					_ => bmr
+				};
+				return amr;
+            }
 		}
 	}
 }

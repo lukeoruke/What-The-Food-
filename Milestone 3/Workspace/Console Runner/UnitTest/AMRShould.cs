02d629844dadeb9e536001d8d936d190bef0bf85
alexyung <48598834+alexyung@users.noreply.github.com>
2022-02-18 00:15:17 -0800

    more unit tests

--- Milestone 3/Workspace/Console Runner/UnitTest/AMRShould.cs
+++ Milestone 3/Workspace/Console Runner/UnitTest/AMRShould.cs
@@ -19,7 +19,6 @@ namespace UnitTest
             // Act
             AMR validNonCustomCase = new AMR(true, 100, 630.2001f, 20, ActivityLevel.None);
             AMR validCustomCase = new AMR(true, 40, 230.2001f, 1000, ActivityLevel.Moderate, 200f);
-            AMR negativeValueCase = new AMR(false, -10, 22.1f, 15, ActivityLevel.Heavy);
 
             // Assert
             Assert.True(validNonCustomCase.IsMale);
@@ -36,6 +35,59 @@ namespace UnitTest
             Assert.Equal(ActivityLevel.Moderate, validCustomCase.Activity);
             Assert.True(validCustomCase.IsCustomAMR);
             Assert.Equal(200f, validCustomCase.CustomAMR);
+        }
+
+        [Fact]
+        public void RejectNegativeMetrics()
+        {
+            // Arrange
+            int validWeight = 100;
+            int invalidWeight = -10;
+            float validHeight = 250f;
+            float invalidHeight = -40f;
+            int validAge = 45;
+            int invalidAge = -22;
+            AMR validNonCustomCase = new AMR(true, validWeight, validHeight, validAge, ActivityLevel.Moderate);
+            
+            // Act
+            validNonCustomCase.Weight = invalidWeight;
+            validNonCustomCase.Height = invalidHeight;
+            validNonCustomCase.Age = invalidAge;
+
+            // Assert
+            Assert.Equal(validWeight, validNonCustomCase.Weight);
+            Assert.Equal(validHeight, validNonCustomCase.Height);
+            Assert.Equal(validAge, validNonCustomCase.Age);
+        }
+
+        [Fact]
+        public void CalculateProperAMR()
+        {
+            // Arrange
+            AMR nonCustomCase = new AMR(true, 100, 630.2001f, 20, ActivityLevel.None);
+            float nonCustomAMR = (66.47f + (13.75f * 100) + (5.003f * 630.2001f) - (6.755f * 20)) * 1.2f;
+
+            // Act
+            float returnedNonCustomAMR = nonCustomCase.CalculateAMR();
+
+            // Assert
+            Assert.Equal(nonCustomAMR, returnedNonCustomAMR);
+        }
+
+        [Fact]
+        public void ReturnCustomAMRWhenAppropriate()
+        {
+            // Arrange
+            AMR customCase = new AMR(true, 40, 230.2001f, 1000, ActivityLevel.Moderate, 200f);
+            float nonCustomAMR = (66.47f + (13.75f * 40) + (5.003f * 230.2001f) - (6.755f * 1000)) * 1.55f;
+            float customAMR = 200f;
+
+            // Act
+            float returnedCustomAMR = customCase.CalculateAMR();
+
+            // Assert
+            Assert.NotEqual(nonCustomAMR, returnedCustomAMR);
+            Assert.Equal(customAMR, returnedCustomAMR);
         }
     }
 }

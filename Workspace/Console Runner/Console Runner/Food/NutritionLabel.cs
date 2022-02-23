b3118843c86f68ed808b2883ac341919d055d00b
TheRealLid <60401858+TheRealLid@users.noreply.github.com>
2022-02-23 08:18:04 -0800

    Merge branch 'Food-Ingredient-Implementation'

---
+++ Workspace/Console Runner/Console Runner/Food/NutritionLabel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food_Class_Library
+{
+    internal class NutritionLabel : INutritionLabel
+    {
+        public NutritionLabel(int calories, int servings, Double servingSize,
+            int totalFat, int saturatedFat, int transFat, int cholestrol, int sodium,
+            int totalCarbohydrate, int dietaryFiber, int totalSugars, int addedSugar,
+            int protein) {
+            
+        }
+
+
+    }
+}

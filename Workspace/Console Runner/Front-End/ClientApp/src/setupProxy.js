dc321b7ae0667ff9f0f8b15dbb34b8068053cdf5
Tythor <tthorin@live.com>
2022-03-01 02:49:47 -0800

    Connected Login to LoginController

--- Workspace/Console Runner/Front-End/ClientApp/src/setupProxy.js
+++ Workspace/Console Runner/Front-End/ClientApp/src/setupProxy.js
@@ -1,18 +1,19 @@
 const createProxyMiddleware = require('http-proxy-middleware');
-const { env } = require('process');
+const {env} = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:29044';
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:29044';
 
-const context =  [
-  "/weatherforecast",
+const context = [
+    '/weatherforecast',
+    '/login',
 ];
 
-module.exports = function(app) {
-  const appProxy = createProxyMiddleware(context, {
-    target: target,
-    secure: false
-  });
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: target,
+        secure: false,
+    });
 
-  app.use(appProxy);
+    app.use(appProxy);
 };
---
+++ Workspace/Console Runner/Front-End/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Front_End.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LoginController : ControllerBase
+{
+    public LoginController()
+    {
+
+    }
+
+    [HttpPost]
+    //public void Post([FromBody] string value)
+    public void Post()
+    {
+        Console.WriteLine("Received Post from LoginController");
+        //Console.WriteLine(Request.Form("username"));
+
+        IFormCollection formData = Request.Form;
+
+        Console.WriteLine(formData["email"]);
+        Console.WriteLine(formData["password"]);
+    }
+}

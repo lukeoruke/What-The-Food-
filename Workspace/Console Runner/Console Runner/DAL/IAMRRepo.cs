eca92744c1c40264beada7bd2eee08a798ab7595
Alexander Dung <Alexander.Dung@student.csulb.edu>
2022-03-01 16:51:18 -0800

    define IAMRRepo

--- Workspace/Console Runner/Console Runner/DAL/IAMRRepo.cs
+++ Workspace/Console Runner/Console Runner/DAL/IAMRRepo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Console_Runner.AMRModel;
 
 namespace Console_Runner.DAL
 {
@@ -11,6 +12,10 @@ namespace Console_Runner.DAL
     /// </summary>
     public interface IAMRRepo
     {
-
+        public bool AMRExists(string email);
+        public AMR? GetAMR(string email);
+        public bool AddAMR(AMR amrToAdd);
+        public bool RemoveAMR(AMR amrToRemove);
+        public bool UpdateAMR(AMR amrToUpdate);
     }
 }

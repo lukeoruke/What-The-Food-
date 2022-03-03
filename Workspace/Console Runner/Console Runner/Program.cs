858c4f5b4b835deee4af9726b6a932be6145f1e3
Alexander Dung <Alexander.Dung@student.csulb.edu>
2022-03-02 16:22:57 -0800

    comment out code using old DAOs

--- Workspace/Console Runner/Console Runner/Program.cs
+++ Workspace/Console Runner/Console Runner/Program.cs
@@ -10,27 +10,8 @@ using LogAndArchive;
 using Console_Runner.DAL;
 using Console_Runner.AMRModel;
 
+/**
 IDataAccess dal = new DummyDaL();
-ILogger log = new Logging();
+ILogger log = new Logging(dal);
 UM um = new UM(dal, log);
-Archiving archiver = new Archiving();
-archiver.archiveStartThread();
-//Role_User user = new Role_User();
-//Console.WriteLine(user.ToString());
-
-
-//Console.WriteLine(adam.ToString());
-
-    Account admin = new Account();
-    admin.Email = "Admin";
-    admin.Password = "pass";
-    //admin.accessLevel = 2;
-    admin.isActive = true;
-    admin.Fname = "matt";
-    admin.Lname = "q";
-    user_permissions permissions = new user_permissions(dal);
-    permissions.defaultAdminPermissions(admin.Email);
-    um.UserSignUp(admin);
-    AMR adminAMR = new AMR(admin, true, 63, 175, 25, ActivityLevel.Daily);
-Console.WriteLine(adminAMR.CalculateAMR());
-    Environment.Exit(0);
+**/

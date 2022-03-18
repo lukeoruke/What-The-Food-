fd0609d572dd8cd839764a77114a59a8937c20c8
Hunter Lewis <40526743+THESUPP0RTER@users.noreply.github.com>
2022-03-18 11:50:48 -0700

    Created Account Class, need to check for constructor values

---
+++ Workspace/Console Runner/Console Runner/Account/Account.cs
@@ -0,0 +1,42 @@
+﻿using Console_Runner;
+using Console_Runner.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Console_Runner.AMRModel;
+
+namespace Console_Runner.Account
+{
+    /*
+     * Account class that will represent the contents of a user's account
+     */
+    public class Account { 
+        //User ID getter and setter
+        [System.ComponentModel.DataAnnotations.Key] //Sets User ID as PK
+        public string UserID{ get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        //User's first name
+        public string FName { get; set; }
+        //User's last name
+        public string LName { get; set; }
+
+        //TODO: Constructor values?
+        public Account()
+        {
+            FName = "";
+            LName = "";
+        }
+
+        public override string ToString()
+        {
+            int pass = this.Password.Length;
+            string stars = "";
+            for (int i = 0; i < pass; i++)
+            {
+                stars += "*";
+            }
+            return this.Email + " " + this.FName + " " + this.LName + " " + stars;
+        }
+    }
+}
---
+++ Workspace/Console Runner/Console Runner/Account/FoodFlag.cs
@@ -0,0 +1,14 @@
+﻿using Console_Runner;
+using Console_Runner.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Console_Runner.AMRModel;
+
+namespace Console_Runner.Account
+{
+    public class FoodFlag
+    {
+
+    }
+}

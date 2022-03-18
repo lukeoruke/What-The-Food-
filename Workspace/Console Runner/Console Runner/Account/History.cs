6c38e603130d5ee254f32913b926eaacd6db0efa
Hunter Lewis <40526743+THESUPP0RTER@users.noreply.github.com>
2022-03-18 12:20:53 -0700

    Added History Class

---
+++ Workspace/Console Runner/Console Runner/Account/History.cs
@@ -0,0 +1,27 @@
+﻿using Console_Runner.DAL;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Console_Runner.Account
+{
+    public class History
+    {
+        public string UserID { get; set; }
+        public string Barcode { get; set; }
+        //TODO: Implement UTC timestamp retrieval
+        public long Timestamp { get; set; }
+
+        public History(string userID, string barcode)
+        {
+            this.UserID = userID;
+            this.Barcode = barcode;
+            //Current timestamp when creating a history object
+            this.Timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
+        }
+
+        public bool AddHistoryItem()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

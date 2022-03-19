8081c19dd7c0ee00454bc1bdf5e523af315d45c9
LukeLS1 <72823928+LukeLS1@users.noreply.github.com>
2022-03-19 11:21:48 -0700

    Update LabelIngredient.cs

--- Workspace/Console Runner/Console Runner/Food/LabelIngredient.cs
+++ Workspace/Console Runner/Console Runner/Food/LabelIngredient.cs
@@ -7,31 +7,33 @@ using System.Threading.Tasks;
 
 
 //TODO: this creates redundency. Why make the same food item over and over again(MQ)
-namespace Console_Runner.Food
+namespace Console_Runner.FoodService;
+
+//TODO: Perhaps add abstraction to this class. What should we add to increase security of an Ingredient object
+public class LabelIngredient
 {
-    //TODO: Perhaps add abstraction to this class. What should we add to increase security of an Ingredient object
-    public class LabelIngredient
+
+    //Property Implementation
+    [ForeignKey("Barcode")]
+    public FoodItem Barcode { get; set; } = new FoodItem();
+    //public string Barcode { get; set; }
+
+    public string IngredientID { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+    public string IngredientsPercentage { get; set; }
+
+    //Constructor
+    public LabelIngredient()
     {
 
-        //Property Implementation
-        public string Barcode { get; set; }
-
-        public string IngredientID { get; set; }
-
-        public string IngredientsPercentage { get; set; }
-
-        //Constructor
-        public LabelIngredient()
-        {
-
-        }
-
-        public LabelIngredient(string barcode, string ingredientID, string percent)
-        {
-            Barcode = barcode;
-            IngredientID = ingredientID;
-            IngredientsPercentage = percent;
-        }
-
     }
+
+    public LabelIngredient(string barcode, string ingredientID, string percent)
+    {
+        Barcode = barcode;
+        IngredientID = ingredientID;
+        IngredientsPercentage = percent;
+    }
+
 }

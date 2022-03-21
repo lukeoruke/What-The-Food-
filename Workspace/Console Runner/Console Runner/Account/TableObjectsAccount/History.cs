9a5df58b8b5d0acca28fb022cc4667c5d398cd98
Matthew R Quinn <MatthewRileyQuinn@gmail.com>
2022-03-21 12:58:39 -0700

    Removed all unneeded "using _____"

--- Workspace/Console Runner/Console Runner/Account/TableObjectsAccount/History.cs
+++ Workspace/Console Runner/Console Runner/Account/TableObjectsAccount/History.cs
@@ -1,6 +1,4 @@
-﻿using Console_Runner.DAL;
-using System;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Console_Runner.AccountService
 {

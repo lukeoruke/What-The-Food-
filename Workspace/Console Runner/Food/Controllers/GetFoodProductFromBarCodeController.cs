281c39b800e1d9106b53a185e5a0e15a48db18c1
Matthew R Quinn <MatthewRileyQuinn@gmail.com>
2022-04-01 17:40:43 -0700

    created jank foodinformation page files

---
+++ Workspace/Console Runner/Food/Controllers/GetFoodProductFromBarCodeController.cs
@@ -0,0 +1,28 @@
+﻿using Console_Runner.FoodService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Food.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetFoodProductFromBarCodeController : ControllerBase
+    {
+        private readonly IFoodGateway _foodServiceGateway = new EFFoodGateway();
+        private readonly FoodDBOperations _foodDB;
+        private string barcode;
+        public GetFoodProductFromBarCodeController()
+        {
+            _foodDB = new FoodDBOperations(_foodServiceGateway);
+            IFormCollection formData = Request.Form;
+            barcode = formData["barcode"];
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<FoodItem>> GET()
+        {
+            return await _foodDB.GetScannedItemAsync(barcode);
+        }
+    }
+}

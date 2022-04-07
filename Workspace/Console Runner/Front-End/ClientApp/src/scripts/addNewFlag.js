fbd9f551faf40f8598d37eb5c65edda918101cf7
Matthew R Quinn <MatthewRileyQuinn@gmail.com>
2022-04-07 16:42:16 -0700

    Added FlagPage and Flag.js

---
+++ Workspace/Console Runner/Front-End/ClientApp/src/scripts/addNewFlag.js
@@ -0,0 +1,17 @@
+﻿namespace Front_End.ClientApp.src.scripts
+{
+    var label = document.createElement("label");
+    var description = document.createTextNode(pair);
+    var checkbox = document.createElement("input");
+
+    checkbox.type = "checkbox";    // make the element a checkbox
+    checkbox.name = "slct[]";      // give it a name we can check on the server side
+    checkbox.value = pair;         // make its value "pair"
+
+    label.appendChild(checkbox);   // add the box to the element
+    label.appendChild(description);// add the description to the element
+
+    // add the label element to your div
+    document.getElementById('box1').appendChild(label);
+    document.getElementById('box1').innerHTML = 'box1213123123';
+}

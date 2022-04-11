543b2c656ad457f26e565387c37b6a07b5acdab1
alexyung <48598834+alexyung@users.noreply.github.com>
2022-04-11 00:00:59 -0700

    Revert "make logging async"
    This reverts commit 7356b606415760855d0e7b54b2042d8f072c0b9d.

--- Workspace/Console Runner/Console Runner/Logging/LogService.cs
+++ Workspace/Console Runner/Console Runner/Logging/LogService.cs
@@ -1,25 +1,14 @@
-﻿using Console_Runner.Logging.Testing;
-namespace Console_Runner.Logging
+﻿namespace Console_Runner.Logging
 {
-    public static class LogService
+    public class LogService
     {
-        //TODO: read DataStoreType as a value from a config file instead of hardcoding
-        private const string DataStoreType = "EF";
-        private static ILogGateway _logAccess;
-        private static IUserIDGateway _userIDAccess;
+        private ILogGateway _logAccess;
+        private IUserIDGateway _userIDAccess;
         //logging objects
-        static LogService()
+        public LogService(ILogGateway logAccessor, IUserIDGateway uidAccessor)
         {
-            if(DataStoreType == "EF")
-            {
-                _logAccess = new EFLogGateway();
-                _userIDAccess = new EFUserIdentifierGateway();
-            }
-            else
-            {
-                _logAccess = new MemLogGateway();
-                _userIDAccess = new MemUserIdentifierGateway();
-            }
+            _logAccess = logAccessor;
+            _userIDAccess = uidAccessor;
         }
 
         /// <summary>
@@ -32,7 +21,7 @@ namespace Console_Runner.Logging
         /// <param name="message">A string containing a message to be included with the log entry.</param>
         /// <param name="timeout">The time in milliseconds that are allowed to elapse before the log attempt is considered failed.</param>
         /// <returns>A Log object representing the log entry to be written to the database.</returns>
-        public static async Task<Log> WriteLogAsync(string actorID, LogLevel level, Category category, DateTime timestamp, string message, int timeout = -1)
+        public async Task<Log> WriteLogAsync(string actorID, LogLevel level, Category category, DateTime timestamp, string message, int timeout = -1)
         {
             CancellationTokenSource cts = new CancellationTokenSource();
             try

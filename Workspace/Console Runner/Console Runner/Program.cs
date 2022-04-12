61cde6a37620dec5e7314abaf920ec84f73620d3
Alexander Dung <Alexander.Dung@student.csulb.edu>
2022-04-12 01:18:42 -0700

    added sample code for adding account to db

--- Workspace/Console Runner/Console Runner/Program.cs
+++ Workspace/Console Runner/Console Runner/Program.cs
@@ -4,14 +4,22 @@
  */
 using Microsoft.Extensions.DependencyInjection;
 using Console_Runner;
+using Console_Runner.AccountService;
 
-
+/*
+IAccountGateway accountGateway = new EFAccountGateway();
+IAuthorizationGateway authorizationGateway = new EFAuthorizationGateway();
+IFlagGateway flagGateway = new EFFlagGateway();
+AccountDBOperations accountService = new AccountDBOperations(accountGateway, authorizationGateway, flagGateway);
+await accountService.UserSignUpAsync(new Account() { Email = "email@example.com", Password = "password", FName = "Guy", LName = "DudeBro"});
+*/
 
 [STAThread]
 static void Main()
 {
     Console.Write("application starting...");
 }
+
 
 /**
 IDataAccess dal = new DummyDaL();

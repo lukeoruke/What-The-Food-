7dab94abe6d3c5c993537a23ec947f4b24ed6287
alexyung <48598834+alexyung@users.noreply.github.com>
2022-04-16 23:49:12 -0700

    implement success logging in EFFlagGateway

--- Workspace/Console Runner/Console Runner/Account/Account_Implementation_EntityFramework/EFFlagGateway.cs
+++ Workspace/Console Runner/Console Runner/Account/Account_Implementation_EntityFramework/EFFlagGateway.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Console_Runner.Logging;
+
 namespace Console_Runner.AccountService
 { 
     public class EFFlagGateway : IFlagGateway
@@ -14,18 +16,29 @@ namespace Console_Runner.AccountService
         {
             _efContext = new ContextAccountDB();
         }
-        public async Task<bool> AccountHasFlagAsync(int userID, int ingredientID)
+        public async Task<bool> AccountHasFlagAsync(int userID, int ingredientID, LogService? logService = null)
         {
             FoodFlag foodFlag = new(userID, ingredientID);
-            return await _efContext.FoodFlags.FindAsync(foodFlag) != null;
+            var toReturn = await _efContext.FoodFlags.FindAsync(foodFlag) != null;
+            if(logService?.UserID != null)
+            {
+                _ = logService.LogWithSetUserAsync(LogLevel.Info, Category.DataStore, DateTime.Now, 
+                    $"Retrieved food flag for user {userID} and ingredient {ingredientID} - exists? {toReturn}");
+            }
+            return toReturn;
         }
 
-        public async Task<bool> AddFlagAsync(FoodFlag flag)
+        public async Task<bool> AddFlagAsync(FoodFlag flag, LogService? logService = null)
         {
             try
             {
-               await _efContext.FoodFlags.AddAsync(flag);
-               await _efContext.SaveChangesAsync();
+                await _efContext.FoodFlags.AddAsync(flag);
+                await _efContext.SaveChangesAsync();
+                if (logService?.UserID != null)
+                {
+                    _ = logService.LogWithSetUserAsync(LogLevel.Info, Category.DataStore, DateTime.Now,
+                        $"Created food flag for user {flag.UserID} and ingredient {flag.IngredientID}");
+                }
                 return true;
             }
             catch (Exception ex)
@@ -34,7 +47,7 @@ namespace Console_Runner.AccountService
             }
         }
 
-        public List<FoodFlag> GetAllAccountFlags(int userID)
+        public List<FoodFlag> GetAllAccountFlags(int userID, LogService? logService = null)
         {
             List<FoodFlag> flagList = new List<FoodFlag>();
             foreach (var flag in _efContext.FoodFlags)
@@ -44,16 +57,26 @@ namespace Console_Runner.AccountService
                     flagList.Add(flag);
                 }
             }
+            if (logService?.UserID != null)
+            {
+                _ = logService.LogWithSetUserAsync(LogLevel.Info, Category.DataStore, DateTime.Now,
+                    $"Retrieved all food flags for user {userID}");
+            }
             return flagList;
         }
 
-        public async Task<bool> RemoveFoodFlagAsync(int userID, int ingredientID)
+        public async Task<bool> RemoveFoodFlagAsync(int userID, int ingredientID, LogService? logService = null)
         {
             if (await AccountHasFlagAsync(userID, ingredientID))
             {
                 FoodFlag foodFlag = new(userID, ingredientID);
                 _efContext.FoodFlags.Remove(foodFlag);
                 await _efContext.SaveChangesAsync();
+                if (logService?.UserID != null)
+                {
+                    _ = logService.LogWithSetUserAsync(LogLevel.Info, Category.DataStore, DateTime.Now,
+                        $"Removed food flag for user {userID} and ingredient {ingredientID}");
+                }
                 return true;
             }
             return false;

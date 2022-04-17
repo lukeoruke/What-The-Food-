368f892cd58ce61bd739a3781612b8fdff3a624a
alexyung <48598834+alexyung@users.noreply.github.com>
2022-04-16 23:49:01 -0700

    add logservice to account gateway interfaces

--- Workspace/Console Runner/Console Runner/Account/AccountInterfaces/IAuthorizationGateway.cs
+++ Workspace/Console Runner/Console Runner/Account/AccountInterfaces/IAuthorizationGateway.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using Console_Runner.Logging;
 
 
 namespace Console_Runner.AccountService
@@ -29,20 +29,20 @@ namespace Console_Runner.AccountService
         /// <param name="userID">The specified Account's userID</param>
         /// <param name="Permission">The associated Permission to check for</param>
         /// <returns>True if the Account has the specified Permission, false otherwise.</returns>
-        public Task<bool> HasPermissionAsync(int userID, string permission);
+        public Task<bool> HasPermissionAsync(int userID, string permission, LogService? logService = null);
 
         /// <summary>
         /// Adds a Permission to the database.
         /// </summary>
         /// <param name="PermissionsToAdd">The Permission to add to the database</param>
         /// <returns>True if the Permission was successfully added to the database, false otherwise.</returns>
-        public Task<bool> AddPermissionAsync(Authorization permissionToAdd);
+        public Task<bool> AddPermissionAsync(Authorization permissionToAdd, LogService? logService = null);
         /// <summary>
         /// Adds a set of Permissions to the database.
         /// </summary>
         /// <param name="permissionsToAdd">The List of Permissions to add to the database</param>
         /// <returns>True if the Permissions in the List were successfully added to the database, false otherwise.</returns>
-        public Task<bool> AddPermissionsAsync(List<Authorization> permissionsToAdd);
+        public Task<bool> AddPermissionsAsync(List<Authorization> permissionsToAdd, LogService? logService = null);
 
         /// <summary>
         /// Removes a specified Permission from the specified Account.
@@ -50,37 +50,37 @@ namespace Console_Runner.AccountService
         /// <param name="userID">The specified Account's userID</param>
         /// <param name="permissions">The associated permissions to remove</param>
         /// <returns>True if the operation was successful, false otherwise.</returns>
-        public Task<bool> RemovePermissionsAsync(int userID, string permissions);
+        public Task<bool> RemovePermissionsAsync(int userID, string permissions, LogService? logService = null);
 
         /// <summary>
         /// Gets a list of all Permissions associated with an Account.
         /// </summary>
         /// <param name="userID">The specified Account's userID</param>
         /// <returns>A List containing all User_Permissions associated with the specified Account.</returns>
-        public List<Authorization> GetAllUserPermissions(int userID);
+        public List<Authorization> GetAllUserPermissions(int userID, LogService? logService = null);
 
         /// <summary>
         /// Removes all Permissions associated with a specified Account.
         /// </summary>
         /// <param name="userID">The specified Account's userID</param>
         /// <returns>True if the operation was successful, false otherwise.</returns>
-        public bool RemoveAllUserPermissions(int userID);
+        public bool RemoveAllUserPermissions(int userID, LogService? logService = null);
         
         /// <summary>
         /// Counts the number of Accounts on the database currently assigned as administrators.
         /// </summary>
         /// <returns>An int reflecting the number of Accounts on the database with administrator permissions.</returns>
-        public int AdminCount();
+        public int AdminCount(, LogService? logService = null);
 
         /// <summary>
         /// Determines if a specified Account is assigned as an administrator in the database.
         /// </summary>
         /// <param name="userID">The specified Account's userID</param>
         /// <returns>True if the specified Account is an administrator, false otherwise.</returns>
-        public bool IsAdmin(int userID);
+        public bool IsAdmin(int userID, LogService? logService = null);
 
-        public Task<bool> AssignDefaultUserPermissions(int userID);
-        public Task<bool> AssignDefaultAdminPermissions(int userID);
+        public Task<bool> AssignDefaultUserPermissions(int userID, LogService? logService = null);
+        public Task<bool> AssignDefaultAdminPermissions(int userID, LogService? logService = null);
 
     }
 }
--- Workspace/Console Runner/Console Runner/Account/AccountInterfaces/IFlagGateway.cs
+++ Workspace/Console Runner/Console Runner/Account/AccountInterfaces/IFlagGateway.cs
@@ -1,12 +1,12 @@
-﻿
+﻿using Console_Runner.Logging;
 
 namespace Console_Runner.AccountService
 {
     public interface IFlagGateway
     {
-        public Task<bool> AccountHasFlagAsync(int userID, int ingredientID);
-        public Task<bool> RemoveFoodFlagAsync(int userID, int ingredientID);
-        public List<FoodFlag> GetAllAccountFlags(int userID);
-        public Task<bool> AddFlagAsync(FoodFlag flag);
+        public Task<bool> AccountHasFlagAsync(int userID, int ingredientID, LogService? logService = null);
+        public Task<bool> RemoveFoodFlagAsync(int userID, int ingredientID, LogService? logService = null);
+        public List<FoodFlag> GetAllAccountFlags(int userID, LogService? logService = null);
+        public Task<bool> AddFlagAsync(FoodFlag flag, LogService? logService = null);
     }
 }

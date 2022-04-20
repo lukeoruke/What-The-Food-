3b558a2f75462216e9bc67e934e5230ff79e0f1a
alexyung <48598834+alexyung@users.noreply.github.com>
2022-04-20 00:52:01 -0700

    define gateway interface

---
+++ Workspace/Console Runner/Console Runner/Food/FoodInterfaces/IFoodUpdateGateway.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Console_Runner.Logging;
+
+namespace Console_Runner.FoodService
+{
+    public interface IFoodUpdateGateway
+    {
+        public Task<bool> AddFoodUpdateAsync(FoodUpdate foodUpdate, LogService? logger = null);
+        public Task<List<FoodUpdate>> GetFoodUpdatesByBarcodeAsync(string barcode, LogService? logger = null);
+        public Task<bool> UpdateFoodUpdateAsync(FoodUpdate foodUpdate, LogService? logger = null);
+        public Task<bool> RemoveFoodUpdate(FoodUpdate foodUpdate, LogService? logger = null);
+        public Task<bool> RemoveFoodUpdateByID(int updateID, LogService? logger = null);
+    }
+}

b518afc5a4e0f14b35c30f6de7dbecd643593010
studentdaramirez0803 <72823669+studentdaramirez0803@users.noreply.github.com>
2022-04-21 17:29:03 -0700

    FRONT END FIXED FOR IIS DEPLOYMENT
    :confetti_ball: :confetti_ball: :confetti_ball:
    Had to remove and move a lot of files.

--- Workspace/Console Runner/Front-End/Program.cs
+++ Workspace/Console Runner/Front-End/Program.cs
@@ -1,23 +1,26 @@
-using Front_End;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Startup
-{
-    public void Configure(IApplicationBuilder app)
-    {
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    }
-}
-public class Program
-{
-    static void Main(string[] args)
-    {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+// Add services to the container.
+builder.Services.AddRazorPages();
 
-            host.Run();
-    }
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();

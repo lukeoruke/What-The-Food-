7d2870b604c8b1075abc54977f0073d911705151
alexyung <48598834+alexyung@users.noreply.github.com>
2022-04-26 23:18:00 -0700

    add logging to food and foodUpdate adds

--- Workspace/Console Runner/Console Runner/Program.cs
+++ Workspace/Console Runner/Console Runner/Program.cs
@@ -15,16 +15,19 @@ async static void Main()
 
 
 /*Console.WriteLine("Program running...");
+LogService logger = LogServiceFactory.GetLogService(LogServiceFactory.DataStoreType.EntityFramework);
+logger.DefaultTimeOut = 5000;
+logger.UserID = "user";
 FoodDBOperations foodDBOperations = new FoodDBOperations(new EFFoodGateway(), new EFFoodUpdateGateway());
 FoodItem food1 = new FoodItem("barcode", "monster", "who what", "link to pic");
 FoodItem food2 = new FoodItem("anotherbarcode", "choccy milk", "who what", "link to pic");
 FoodItem food3 = new FoodItem("athirdbarcode", "tea", "some dude", "link to pic");
-foodDBOperations.AddFoodItemAsync(food1).Wait();
-foodDBOperations.AddFoodItemAsync(food2).Wait();
-foodDBOperations.AddFoodItemAsync(food3).Wait();
-await foodDBOperations.AddFoodUpdateAsync(new FoodIngredientChange(food1, DateTime.Now, "ing change for monster", new[] { new Ingredient("some ing", "whatt", "iunno") }, new[] { new Ingredient("removed ign", "????", "fuck") }));
-await foodDBOperations.AddFoodUpdateAsync(new FoodRecall(food1, DateTime.Now, "sermonella", new[] { "location" }, new[] { 10 }, new[] { DateTime.Parse("2020-01-03") }));
-await foodDBOperations.AddFoodUpdateAsync(new FoodRecall(food2, DateTime.Now, "norovirus!", new[] { "otherlocation" }, new int[0], new[] { DateTime.Parse("2020-01-23") }));
+foodDBOperations.AddFoodItemAsync(food1, logger).Wait();
+foodDBOperations.AddFoodItemAsync(food2, logger).Wait();
+foodDBOperations.AddFoodItemAsync(food3, logger).Wait();
+await foodDBOperations.AddFoodUpdateAsync(new FoodIngredientChange(food1, DateTime.Now, "ing change for monster", new[] { new Ingredient("some ing", "whatt", "iunno") }, new[] { new Ingredient("removed ign", "????", "fuck") }), logger);
+await foodDBOperations.AddFoodUpdateAsync(new FoodRecall(food1, DateTime.Now, "sermonella", new[] { "location" }, new[] { 10 }, new[] { DateTime.Parse("2020-01-03") }), logger);
+await foodDBOperations.AddFoodUpdateAsync(new FoodRecall(food2, DateTime.Now, "norovirus!", new[] { "otherlocation" }, new int[0], new[] { DateTime.Parse("2020-01-23") }), logger);
 */
 
 /*Console.WriteLine("Program running...");

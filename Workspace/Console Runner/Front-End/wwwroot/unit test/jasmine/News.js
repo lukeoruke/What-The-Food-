e00ccf8f57e3603471e0d0805a25fce43abbb0ad
LukeLS1 <72823928+LukeLS1@users.noreply.github.com>
2022-04-28 15:21:48 -0700

    aggregated news updated

---
+++ Workspace/Console Runner/Front-End/wwwroot/unit test/jasmine/News.js
@@ -0,0 +1,25 @@
+﻿News = function() {};
+
+
+News.prototype.incrementHealthFilter = function (bias) {
+    this.bias = bias + 1;
+    return (this.bias);
+}
+News.prototype.decrementHealthFilter = function (bias) {
+    this.bias = bias - 1;
+    return (this.bias);
+}
+News.prototype.storeFoodNews = function (displayAmnt) {
+    var array = [];
+    for (; displayAmnt < 5; displayAmnt++) {
+        array.push("FoodNews" + displayAmnt);
+    }
+    return true;
+}
+News.prototype.storeHealthNews = function (displayAmnt) {
+    var array = [];
+    for (var x = 0; x < displayAmnt; x++) {
+        array.push("HealthNews" + x);
+    }
+    return true;
+}

c9e67e19c3965955647c38d18725ea31e48ecbba
Hunter Lewis <thesupprter@gmail.com>
2022-04-28 23:32:35 -0700

    before migration deletion

--- Workspace/Console Runner/Front-End/ClientApp/src/scripts/graph.js
+++ Workspace/Console Runner/Front-End/ClientApp/src/scripts/graph.js
@@ -1,4 +1,35 @@
-﻿async function getFoodObject(e) {
+﻿/**
+ * Render graph from chart.js
+ */
+
+const GRAPH = document.getElementById("testGraph");
+
+
+//Create array that will store all graph data objects
+let graphs = [];
+
+let barGraph = new Chart(GRAPH, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            data: [20, 10],
+        }],
+        labels: ['a', 'b']
+    }
+})
+
+console.log(GRAPH);
+const push = document.getElementById('push');
+push.addEventListener('click', populateBarGraph);
+function populateBarGraph() {
+    const pushValue = document.getElementById('pushValue');
+    barGraph.data.datasets[0].data.push(pushValue.value);
+    console.log(barGraph.data.datasets[0].data.push(pushValue.value));
+    
+
+}
+
+async function getFoodObject(e) {
     e.preventDefault();
 
     console.log('Attempting to Scan...');
@@ -8,13 +39,11 @@
     //JSON.stringify(formData);
     console.log(barcode);
 
-
-
     // HTTP Get Request
     await fetch('http://localhost:49200/api/GetFoodProductFromBarCode?' + barcode)
         .then(async response => localStorage.setItem('foodInfo', JSON.stringify(await response.json())))
         .then(data => console.log(data));
-
+    
     // HTTP Post Request
     await fetch('http://localhost:49200/api/GetFoodProductFromBarCode', {
         method: 'POST',
@@ -22,6 +51,7 @@
     }).then(function (response) {
         console.log(response.status); // returns 200;
     });
-
-    window.location.replace("/foodInformation.html");
 }
+
+
+

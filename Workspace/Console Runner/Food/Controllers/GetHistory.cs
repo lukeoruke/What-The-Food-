b3338e03c27913ee2c1b2dbe56d4e6380c39b8f7
Hunter Lewis <thesupprter@gmail.com>
2022-04-29 03:34:10 -0700

    Unit Tests

---
+++ Workspace/Console Runner/Food/Controllers/GetHistory.cs
@@ -0,0 +1,39 @@
+﻿using Console_Runner.Account.Account_Implementation_EntityFramework;
+using Console_Runner.Account.AccountInterfaces;
+using Console_Runner.AccountService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Food.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetHistory : Controller
+    {
+        private readonly IAccountNews efNews = new EFNews();
+        private AccountDBOperations _dbOperations;
+        int userID = 1; //TODO: Needs JWT Token
+        [HttpGet]
+        /// <summary>
+        /// Gets the Number of Health News that should be displayed
+        /// </summary>
+        /// <returns>Returns number of health news to display</returns>
+        /// <exception cref="Exception">Throw if get call is unreachable</exception>
+        public async Task<ActionResult<int>> Get()
+        {
+            try
+            {
+               
+            }
+            catch (Exception e)
+            {
+               Console.WriteLine(e);
+
+            }
+
+
+        }
+
+    }
+
+}
+
--- Workspace/Console Runner/Front-End/ClientApp/src/scripts/graph.js
+++ Workspace/Console Runner/Front-End/ClientApp/src/scripts/graph.js
@@ -8,19 +8,21 @@ const GRAPH = document.getElementById("testGraph");
 //Create array that will store all graph data objects
 let graphs = [];
 
-let barGraph = new Chart(GRAPH, {
-    type: 'bar',
-    data: {
-        datasets: [{
-            data: [20, 10],
-        }],
-        labels: ['a', 'b']
-    }
-})
+//let barGraph = new Chart(GRAPH, {
+//    type: 'bar',
+//    data: {
+//        datasets: [{
+//            data: [20, 10],
+//        }],
+//        labels: ['a', 'b']
+//    }
+//})
 
-console.log(GRAPH);
-const push = document.getElementById('push');
-push.addEventListener('click', populateBarGraph);
+//console.log(GRAPH);
+//const push = document.getElementById('push');
+//push.addEventListener('click', populateBarGraph);
+
+
 function populateBarGraph() {
     const pushValue = document.getElementById('pushValue');
     barGraph.data.datasets[0].data.push(pushValue.value);
@@ -52,6 +54,16 @@ async function getFoodObject(e) {
         console.log(response.status); // returns 200;
     });
 }
+
+async function getHistory(e) {
+    e.preventDefault();
+
+    await fetch('http://localhost:49200/api/GetHistory',{
+        method: 'GET'
+    })
+}
+        
+
 
 
 

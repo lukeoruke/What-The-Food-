a4e42121c216f3ec68b1be917a5c169fa3f3ed1c
Hunter Lewis <thesupprter@gmail.com>
2022-05-03 12:04:45 -0700

    Let's run this back

---
+++ Workspace/Console Runner/AccountMicroservice/Controllers/Authentication/IAuthenticationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Runner.AccountService.Authentication
+{
+    public interface IAuthenticationService
+    {
+        public string GenerateToken(string data);
+        public string Decrypt(string encryptedData);
+        public bool ValidateToken(string token);
+        public string GetUsername(string token);
+    }
+}

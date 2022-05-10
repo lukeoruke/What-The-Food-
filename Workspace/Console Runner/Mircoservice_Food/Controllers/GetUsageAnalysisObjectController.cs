e18dedcd4766d5ae073a78e6683bf36e254af4a0
Alexander Dung <Alexander.Dung@student.csulb.edu>
2022-05-10 16:03:42 -0700

    getusageanalysisobjectcontroller now needs an admin token for request

--- Workspace/Console Runner/Mircoservice_Food/Controllers/GetUsageAnalysisObjectController.cs
+++ Workspace/Console Runner/Mircoservice_Food/Controllers/GetUsageAnalysisObjectController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 
+using Console_Runner.AccountService;
 using Console_Runner.Logging;
 
 namespace Mircoservice_Food.Controllers
@@ -10,31 +11,63 @@ namespace Mircoservice_Food.Controllers
     public class GetUsageAnalysisObjectController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<string>> Get() {
+        public async Task<ActionResult<string>> Get(string token) {
             LogService logger = LogServiceFactory.GetLogService(LogServiceFactory.DataStoreType.EntityFramework);
+            AccountDBOperations accountService = new AccountDBOperations(new EFAccountGateway(), new EFAuthorizationGateway(), new EFFlagGateway(), new EFAMRGateway(), new EFActiveSessionTrackerGateway());
 
-            try
+            if (await accountService.ValidateToken(token))
             {
-                var loginTrend = logger.GetLoginTrends(DateTime.Now.AddMonths(-3));
-                var signupTrend = logger.GetSignupTrends(DateTime.Now.AddMonths(-3));
-                var mostViewedPages = logger.GetMostViewedPages();
-                var highestAverageDurationPages = logger.GetHighestAverageDurationPages();
-                var mostScannedBarcodes = logger.GetMostScannedBarcodes();
-                var mostFlaggedIngredients = logger.GetMostFlaggedIngredients();
-                return JsonSerializer.Serialize(new
+                int activeUserId = await accountService.GetActiveUserAsync(token);
+                Account? activeUser = await accountService.GetUserAccountAsync(activeUserId);
+                if(activeUser?.CollectData ?? false)
                 {
-                    logins = loginTrend,
-                    signups = signupTrend,
-                    mostViewedPages = mostViewedPages,
-                    highestAverageDurationPages = highestAverageDurationPages,
-                    mostScannedBarcodes = mostScannedBarcodes,
-                    mostFlaggedIngredients = mostFlaggedIngredients
-                });
+                    logger.UserEmail = activeUser!.Email;
+                }
+                else
+                {
+                    logger.UserEmail = "Unknown";
+                }
+                if (accountService.IsAdmin(activeUserId))
+                {
+                    try
+                    {
+                        var loginTrend = logger.GetLoginTrends(DateTime.Now.AddMonths(-3));
+                        var signupTrend = logger.GetSignupTrends(DateTime.Now.AddMonths(-3));
+                        var mostViewedPages = logger.GetMostViewedPages();
+                        var highestAverageDurationPages = logger.GetHighestAverageDurationPages();
+                        var mostScannedBarcodes = logger.GetMostScannedBarcodes();
+                        var mostFlaggedIngredients = logger.GetMostFlaggedIngredients();
+                        _ = logger.LogWithSetUserAsync(Console_Runner.Logging.LogLevel.Info, Category.Business, DateTime.Now,
+                                                   $"User requested usage analysis metrics.");
+                        return JsonSerializer.Serialize(new
+                        {
+                            logins = loginTrend,
+                            signups = signupTrend,
+                            mostViewedPages = mostViewedPages,
+                            highestAverageDurationPages = highestAverageDurationPages,
+                            mostScannedBarcodes = mostScannedBarcodes,
+                            mostFlaggedIngredients = mostFlaggedIngredients
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        _ = logger.LogWithSetUserAsync(Console_Runner.Logging.LogLevel.Error, Category.Business, DateTime.Now,
+                                                   $"User requested usage analysis metrics. Unknown error: {ex.Message}");
+                        return new StatusCodeResult(500);
+                    }
+                }
+                else
+                {
+                    _ = logger.LogWithSetUserAsync(Console_Runner.Logging.LogLevel.Info, Category.Business, DateTime.Now,
+                                               $"User {activeUserId} is unauthorized to request usage analysis metrics.");
+                    return new UnauthorizedResult();
+                }
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine(ex.ToString());
-                return new StatusCodeResult(500);
+                _ = logger.LogAsync("Unknown", Console_Runner.Logging.LogLevel.Debug, Console_Runner.Logging.Category.Server, DateTime.Now,
+                                    $"Received invalid JWT token.");
+                return new UnprocessableEntityResult();
             }
         
         }

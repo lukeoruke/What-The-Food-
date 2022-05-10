9a1a7d9ba037d6e81e79fd7949f1796aa940e972
Alexander Dung <Alexander.Dung@student.csulb.edu>
2022-05-09 22:46:07 -0700

    front end logging first pass

--- Workspace/Console Runner/Mircoservice_Food/Controllers/ValidateAdminLoggedInController.cs
+++ Workspace/Console Runner/Mircoservice_Food/Controllers/ValidateAdminLoggedInController.cs
@@ -1,5 +1,6 @@
 ﻿using Console_Runner.AccountService;
 using Console_Runner.AccountService.Authentication;
+using Console_Runner.Logging;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mircoservice_Food.Controllers
@@ -16,12 +17,53 @@ namespace Mircoservice_Food.Controllers
         private readonly IAuthenticationService _JWTAuthenticationService = new JWTAuthenticationService("TESTDATAHERE");
 
         [HttpGet]
-        public async Task<ActionResult<bool>> Get(string token)
+        public async Task<ActionResult<bool>> Get(string token, int time, string previousViewName, string currentViewName)
         {
             AccountDBOperations _accountDBOperations = new AccountDBOperations(_accountAccess, _permissionService, _flagGateway, _amRGateway, _EFActiveSessionTrackerGateway);
+            LogService logger = LogServiceFactory.GetLogService(LogServiceFactory.DataStoreType.EntityFramework);
+            logger.DefaultTimeOut = 5000;
             bool tokenIsValid = await _accountDBOperations.ValidateToken(token);
-            int tokenId = await _accountDBOperations.GetActiveUserAsync(token);
-            bool tokenBelongsToAdmin = _accountDBOperations.IsAdmin(tokenId);
+            int activeUserId = await _accountDBOperations.GetActiveUserAsync(token);
+            bool tokenBelongsToAdmin = _accountDBOperations.IsAdmin(activeUserId);
+            if (tokenIsValid)
+            {
+                Account? activeUser = await _accountDBOperations.GetUserAccountAsync(activeUserId);
+                // token is valid and user is admin
+                if (tokenBelongsToAdmin)
+                {
+                    if (activeUser?.CollectData ?? false)
+                    {
+                        logger.UserEmail = activeUser.Email;
+                        _ = logger.LogWithSetUserAsync(Console_Runner.Logging.LogLevel.Info, Category.View, DateTime.Now,
+                                                       $"Admin {activeUserId} navigated from {previousViewName} after {time} seconds to {currentViewName}.");
+                    }
+                    else
+                    {
+                        _ = logger.LogAsync("Unknown", Console_Runner.Logging.LogLevel.Info, Category.View, DateTime.Now,
+                                            $"Unknown admin navigated from {previousViewName} after {time} seconds to {currentViewName}.");
+                    }
+                }
+                // token is valid and user is not an admin
+                else
+                {
+                    if (activeUser?.CollectData ?? false)
+                    {
+                        logger.UserEmail = activeUser.Email;
+                        _ = logger.LogWithSetUserAsync(Console_Runner.Logging.LogLevel.Warning, Category.View, DateTime.Now,
+                                                       $"Unauthorized user {activeUserId} attempted to navigate from {previousViewName} to {currentViewName}.");
+                    }
+                    else
+                    {
+                        _ = logger.LogAsync("Unknown", Console_Runner.Logging.LogLevel.Warning, Category.View, DateTime.Now,
+                                            $"Unknown unauthorized user attempted to navigate from {previousViewName} to {currentViewName}.");
+                    }
+                }
+            }
+            else
+            {
+                _ = logger.LogAsync("Unknown", Console_Runner.Logging.LogLevel.Warning, Category.View, DateTime.Now,
+                                    $"User attempted to navigate from {previousViewName} to {currentViewName} with an invalid token.");
+            }
             return tokenIsValid && tokenBelongsToAdmin;
         }
     }

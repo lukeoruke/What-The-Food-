f3e333e4930c71e21b83edf4a7e97bd18190022f
Hunter Lewis <thesupprter@gmail.com>
2022-05-09 23:25:36 -0700

    Current changes for localstorage

--- Workspace/Console Runner/Front-End/wwwroot/scripts/VerifyLoggedIn.js
+++ Workspace/Console Runner/Front-End/wwwroot/scripts/VerifyLoggedIn.js
@@ -1,15 +1,41 @@
 ﻿
 
+var previousViewName = '';
+var currentViewName = '';
+var time = 0;
+
+
+//Search localstorage for time and name of view to fetch call
+//Save current time and name of new view to localstorage
+function placeLocalStorage(previousViewName, currentViewName, time) {
+    //If localstorage doesn't contain any information, place viewNames and time
+    if (localStorage.getItem(this.previousViewName != null) {
+        localStorage.setItem(this.previousViewName, previousViewName);
+    }
+    if (localStorage.getItem(this.currentViewName != null){
+        localStorage.setItem(this.currentViewName, currentViewName);
+    }
+    if (localStorage.getItem(this.time != null)) {
+        localStorage.setItem(this.time, Date().getTime());
+    }
+
+
+});
+
+
 (async () => {
     var jwt = localStorage.getItem('JWT');
-
-    await fetch('http://localhost:49202/api/ValidateLoggedIn?' + new URLSearchParams({ token: jwt }))
+    //viewName: string
+    //time: int
+    await fetch('http://localhost:49202/api/ValidateLoggedIn?' + new URLSearchParams({ token: jwt, previousViewName: previousViewName, currentViewName: currentViewName, time: time, }))
         .then(response => response.text())
         .then((response) => {
             if (response === "False") {
                 window.location.replace("https://localhost:49199/login.html");
             }
         })
-
+    
 
 })();
+
+

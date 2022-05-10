65b8d12e4a1868c364bd6f72f0dd56dd425ac6ad
Hunter Lewis <thesupprter@gmail.com>
2022-05-10 03:07:46 -0700

    GetUsageAnalysisController Created

---
+++ Workspace/Console Runner/Mircoservice_Food/Controllers/GetUsageAnalysisObjectController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+using Console_Runner.Logging;
+
+namespace Mircoservice_Food.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetUsageAnalysisObjectController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Get() {
+            LogService logger = LogServiceFactory.GetLogService(LogServiceFactory.DataStoreType.EntityFramework);
+
+            logger.LogListAsync();
+            await logger.LogAsync(logger.UserEmail, Console_Runner.Logging.LogLevel.Info, Category.Data, DateTime.Now,
+                "Received Usage Analysis Data ");
+        
+        }
+
+    }
+}
